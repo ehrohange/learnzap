908f062a065b5eed621153d7b13bbb00042e0fde
ehrohange <jorgeangelopangilinan@gmail.com>
2025-07-11 01:32:07 +0800

    added mailer util

---
+++ backend/src/utils/mailer.ts
@@ -0,0 +1,22 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
+
+export async function sendEmail(to: string, subject: string, html: string) {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS,
+    },
+  });
+
+  const info = await transporter.sendMail({
+    from: `"Your App" <${process.env.GMAIL_USER}>`,
+    to,
+    subject,
+    html,
+  });
+
+  console.log("Email sent:", info.messageId);
+}
